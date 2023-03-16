627aa71abafbd87688a8932f2f94475f8718295b
sofiane <sofyanekebci@gmail.com>
2023-03-16 11:27:23 +0100

    I changed the way I send and receive information

--- projet/src/ClientHandler.java
+++ projet/src/ClientHandler.java
@@ -1,7 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -24,26 +21,24 @@ public class ClientHandler extends Thread {
 
     @Override
     public void run() {
-        PrintWriter out;
-        BufferedReader in;
+        OutputStream out;
         requests[0] =false;
         requests[1] =false;
 
         try {
-            out = new PrintWriter(
-                    socket.getOutputStream(), true);
-            in = new BufferedReader(
-                    new InputStreamReader(
-                            socket.getInputStream()));
+            DataOutputStream os = new DataOutputStream(socket.getOutputStream());
+            DataInputStream is = new DataInputStream(socket.getInputStream());
+
             String line ;
-            while ((line = in.readLine()) != null) {
-                System.out.println(line);
+            while ((line = is.readUTF()) != null){
                 String request;
                 try {
                     request =line.substring(0,line.indexOf("@")).trim();
                 } catch (StringIndexOutOfBoundsException e)  {
                     request="ERROR";
                 }
+                System.out.println(request);
+
                 if (request.equals("PUBLISH") || request.equals("ERROR")) requests[0] =true;
                 else if (request.equals("RCV_IDS")) requests[1]=true;
 
@@ -53,18 +48,21 @@ public class ClientHandler extends Thread {
                     DataBaseRequests dataBaseRequests = new DataBaseRequests();
                     if (username== "") username = line.substring(line.indexOf("@"),line.indexOf("#")).trim();
                     String message = line.substring(line.indexOf("#")+1);
+                    System.out.println(message);
+
                     //DataBaseRequests.updateData("INSERT INTO MESSAGES VALUES(2,'@Mériem','hiifazzzzzzvgiii')");
                     dataBaseRequests.updateData("Insert into MESSAGES values("+
                             dataBaseRequests.findId()+",'"+username+"','"+message+"');");
                     System.out.printf(
                             "-> %s\n",
                             username+": "+message);
-                    out.println("-> ok");
+                    os.writeUTF("Message sent successfully");
                 }
                 else if (requests[1]) {
                     DataBaseRequests dataBaseRequests = new DataBaseRequests();
                      set = dataBaseRequests.selectData("Select* from MESSAGES ORDER BY id DESC limit 5 ;");
-                     out.println(set);
+                     os.writeUTF(set);
+
 
                 }
 
--- projet/src/ClientTCP.java
+++ projet/src/ClientTCP.java
@@ -22,6 +22,7 @@ public class ClientTCP {
         int request;
         String message;
 
+
         do {
             System.out.println("What do you want to do ?");
             System.out.println("1-PUBLISH");
@@ -29,27 +30,28 @@ public class ClientTCP {
             request = Integer.parseInt(scanner.nextLine());
             System.out.println(request);
         } while (request != 1 && request!=2);
+
         DataOutputStream os = new DataOutputStream(s.getOutputStream());
         if (request==1){
             message ="PUBLISH@"+username;
             System.out.println("Your messages : ");
             while(scanner.hasNextLine()){
-                String textInput = scanner.nextLine()+"\n";
-                message =message+"#"+textInput;
+                String textInput = scanner.nextLine();
+                message =message+"#"+textInput+"\n";
                 os.writeUTF(message);
                 message="";
-                BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-                String response = in.readLine();
+                DataInputStream is = new DataInputStream(s.getInputStream());
+                String response = is.readUTF();
                 System.out.println(response);
             }
         }
         else if (request==2){
                 message = "RCV_IDS@" + username+"\n";
                 os.writeUTF(message);
-                BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-                String response = in.readLine();
+                DataInputStream is = new DataInputStream(s.getInputStream());
+                String response = is.readUTF();
                 System.out.println("Here is some IDs");
-               String[] g = response.split("-");
+                String[] g = response.split("-");
                 for (int i =1; i<g.length;i++)
                    System.out.println(g[i]);
             }
